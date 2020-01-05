a228dacb519fd9cb27daac473ebcc63c07b760a6
Lexa <alexej.stadnik@yandex.ru>
2020-01-05 17:24:59 +0300

    Это первый комит

---
+++ lesson1/javascript.js
@@ -0,0 +1,24 @@
+var budget = prompt("Ваш бюджет?");
+var storeName = prompt("Название вашего магазина?");
+
+var shopGoods = ['plum', 'orange', 'apple'];
+
+employers = {
+    marina: "Марина",
+    pavel: "Павел"    
+
+}
+
+mainList = {
+    budget,
+    storeName,
+    shopGoods,
+    employers
+}
+
+console.log(mainList["budget"]);
+
+
+var sale = prompt("Какой тип товаров будем продавать?");
+
+console.log(budget / 30);
